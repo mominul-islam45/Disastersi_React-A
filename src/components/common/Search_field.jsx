e45455b6f7c5dd6372570ddd98615d00b900e891
mominul-islam45 <mominul4586@gmail.com>
2025-02-04 11:41:44 +0600

    incident page step 4 ui added

--- src/components/common/Search_field.jsx
+++ src/components/common/Search_field.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { cn } from "../../lib/utility/cn";
 
-const Search_field = ({placeholder}) => {
+const Search_field = ({placeholder, className}) => {
   return (
     <div className="relative">
       <input
         type="search"
         placeholder={placeholder}
-        className="w-[150px] md:w-[190px] ps-8 p-2 bg-light rounded-[6px] search-bar placeholder:text-xs  border border-gray/15 outline-0 focus:border-orange/50 hover:border-gray/60 transition-all"
+        className={cn("w-[150px] md:w-[190px] py-2 ps-8 bg-light rounded-[6px] placeholder:text-xs  border border-gray/15 outline-0 focus:border-orange/50 hover:border-gray/60 transition-all", className)}
       />
       <img
         src="./icons/search.svg"
--- src/pages/incidents/Step_4.jsx
+++ src/pages/incidents/Step_4.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from '../../components/common/container'
 import Title from '../../components/common/Title'
+import Search_field from '../../components/common/search_field'
 
 const Step_4 = () => {
   return (
@@ -12,8 +13,12 @@ const Step_4 = () => {
           <p className='text-sm text-gray pb-5 leading-[22px]'>Enter a GPS, address. or pin point on the map. Try to be as accurate as possible, or click:Jurisdiction Wide</p>
         </div>
         {/* Location */}
-        <div>
-          
+        <div className='relative rounded-[10px] overflow-clip mb-10'>
+          <div className='space-y-2 absolute left-3 top-3 z-20'>
+            <Search_field className='w-[225px] md:w-[225px]' placeholder='Enter incident address or GPS'/>
+            <Search_field className='w-[225px] md:w-[225px]' placeholder='Pinpoint damage'/>
+          </div>
+          <img src="imgs/location.png" alt="Location" className=' cursor-pointer hover:scale-105 transition-all w-full object-cover'/>
         </div>
       </Container>
     </>
